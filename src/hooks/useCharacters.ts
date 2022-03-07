4c32e6db65a0e290a201a8d660cb959f1606d6b7
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2022-03-07 10:26:03 -0300

    chore(hook): remove console log

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -4,7 +4,6 @@ import { Characters } from '../services/characters/characters.type';
 
 const useCharacters = (nameParam?: string | undefined) => {
   const getNextPageParam = (lastPage: Characters, pages: Characters[]) => {
-    console.log({ lastPage, pages });
     if (lastPage.info.next) {
       return pages.length + 1;
     }
