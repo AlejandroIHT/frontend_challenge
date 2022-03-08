e43550c52988b41cec937f8b3616503a5591e3bf
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2022-03-08 12:36:56 -0300

    feat(test): add test to tag component

---
+++ src/ui/Tag/Tag.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import Tag, { TagType } from './Tag';
+
+describe('Tag component', () => {
+  test('should render', () => {
+    const { container } = render(<Tag type={TagType.PRIMARY}>Tag</Tag>);
+    expect(screen.getByText('Tag')).toBeInTheDocument();
+    expect(container.querySelector('.tag--primary')).toBeInTheDocument();
+  });
+
+  test('should render secondary tag', () => {
+    const { container } = render(<Tag type={TagType.SECONDARY}>Tag</Tag>);
+    expect(screen.getByText('Tag')).toBeInTheDocument();
+    expect(container.querySelector('.tag--secondary')).toBeInTheDocument();
+  });
+
+  test('should render with className', () => {
+    const { container } = render(
+      <Tag type={TagType.PRIMARY} className="test">
+        Tag
+      </Tag>
+    );
+    expect(screen.getByText('Tag')).toBeInTheDocument();
+    expect(container.querySelector('.test')).toBeInTheDocument();
+  });
+});
