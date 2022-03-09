234fa9d114c4b300be172ebc31e55f1c93337489
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2022-03-09 00:21:32 -0300

    feat(ui): create test for action button component

---
+++ src/ui/ActionButton/ActionButton.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import ActionButton, { ActionButtonType } from './ActionButton';
+
+describe('ActionButton component', () => {
+  test('should render', () => {
+    const { container } = render(
+      <ActionButton typeButton={ActionButtonType.PRIMARY}>
+        Action Button
+      </ActionButton>
+    );
+    expect(screen.getByText('Action Button')).toBeInTheDocument();
+    expect(container.querySelector('.action-button')).toBeInTheDocument();
+  });
+
+  test('should render primary button', () => {
+    const { container } = render(
+      <ActionButton typeButton={ActionButtonType.PRIMARY}>
+        Action Button
+      </ActionButton>
+    );
+    expect(screen.getByText('Action Button')).toBeInTheDocument();
+    expect(
+      container.querySelector('.action-button--primary')
+    ).toBeInTheDocument();
+  });
+
+  test('should render secondary button', () => {
+    const { container } = render(
+      <ActionButton typeButton={ActionButtonType.SECONDARY}>
+        Action Button
+      </ActionButton>
+    );
+    expect(screen.getByText('Action Button')).toBeInTheDocument();
+    expect(
+      container.querySelector('.action-button--secondary')
+    ).toBeInTheDocument();
+  });
+
+  test('should render with className', () => {
+    const { container } = render(
+      <ActionButton typeButton={ActionButtonType.PRIMARY} className="test">
+        Action Button
+      </ActionButton>
+    );
+    expect(screen.getByText('Action Button')).toBeInTheDocument();
+    expect(container.querySelector('.test')).toBeInTheDocument();
+  });
+});
