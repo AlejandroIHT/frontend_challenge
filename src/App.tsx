d0acce009742466e7dce5d8c512f1e86182f85b3
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2022-03-09 02:56:48 -0300

    feat(app): add modal state provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import CharactersList from './screen/CharactersList';
 import Layout from './components/Layout';
+import ModalState from './context/ModalContext/ModalState';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <CharactersList />
-      </Layout>
+      <ModalState>
+        <Layout>
+          <CharactersList />
+        </Layout>
+      </ModalState>
     </QueryClientProvider>
   );
 }
