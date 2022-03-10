c22aa18eef41f3f1db88a1bc07be2ab549d98e10
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2022-03-10 00:13:40 -0300

    feat(app): add filter state

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import CharactersList from './screen/CharactersList';
 import Layout from './ui/Layout';
 import ModalState from './context/ModalContext/ModalState';
+import FilterState from './context/FilterContext/FilterState';
 
 const queryClient = new QueryClient();
+queryClient.invalidateQueries();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ModalState>
-        <Layout>
-          <CharactersList />
-        </Layout>
+        <FilterState>
+          <Layout>
+            <CharactersList />
+          </Layout>
+        </FilterState>
       </ModalState>
     </QueryClientProvider>
   );
