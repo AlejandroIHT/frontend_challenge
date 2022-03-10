ebfac1382c1f317c65e952a4190dde306f9c1fdc
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2022-03-09 22:51:30 -0300

    fix(app): change import to layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import CharactersList from './screen/CharactersList';
-import Layout from './components/Layout';
+import Layout from './ui/Layout';
 import ModalState from './context/ModalContext/ModalState';
 
 const queryClient = new QueryClient();
